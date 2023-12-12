e0f4a96cc89ea7fc0c70bf0e9716b5fd26cc27bb
vishnums <shashankmv01@gmail.com>
2023-12-12 10:18:17 +0530

    added backtracking template in ratmaze

--- topic-wise/backtracking/02.ratmaze.py
+++ topic-wise/backtracking/02.ratmaze.py
@@ -1,3 +1,24 @@
+available_directions = [("D",1,0),("R",0,1),("T",-1,0),("L",0,-1)]
+
+"""
+    format for backtracking algorithm is 
+    def backtracking_example(arr,n,r+n_r,c+n_c,output,current_path):
+        if is_finish():
+            pass
+        else:
+            #loop over all possible directions
+            for _ in range(n):
+                #check if that direction is valid
+                if is_valid():
+                    #set some variable
+                    arr[r][c]=-1
+                    current_path += d
+                    backtracking_example(arr,n,r+n_r,c+n_c,output,current_path)
+                    #backtrack revert it
+                    arr[r+n_r][c+n_c] = 1
+                    current_path = current_path[:-1]
+"""
+
 def searchMaze(arr:list[list[int]],n:int)->str:
     """Find a possible solution for RAT to go from Source to Destination
 
@@ -6,9 +27,9 @@ def searchMaze(arr:list[list[int]],n:int)->str:
         n (int): size of input squared array 5 means 5x5 matrix
 
     Returns:
-        str: outputs format like DRDRR
+        str: outputs format like DDRDRR DRDDRR
     >>> searchMaze([[1,0,0,0],[1,1,0,1],[1,1,0,0],[0,1,1,1]],4)
-    'DDRDRR'
+    'DDRDRR DRDDRR'
     >>> searchMaze([[1,0],[1,0]],2)
     ''
     """
@@ -16,36 +37,28 @@ def searchMaze(arr:list[list[int]],n:int)->str:
     if arr[0][0] == 0:
         return output
     searchMazeHelper(arr,n,0,0,output)
-    return "".join(output)
+    return "".join(output).strip()
 
-def is_safe(arr:list[list],n:int,i:int,j:int):
-    if 0<=i<n and 0<=j<n and arr[i][j]==1:
+def is_safe(arr:list[list],n:int,r:int,c:int):
+    if 0<=r<n and 0<=c<n and arr[r][c]==1:
         return True
 
-def searchMazeHelper(arr:list[list],n:int,i:int,j:int,output:list,current_path=""):
-    if i==n-1 and j==n-1:
+def searchMazeHelper(arr:list[list],n:int,r:int,c:int,output:list,current_path=""):
+    if r==n-1 and c==n-1:
         output.append(current_path)
+        output.append(" ")
     else:
-        if is_safe(arr,n,i+1,j):
-            #move down
-            arr[i+1][j]=0
-            searchMazeHelper(arr,n,i+1,j,output,current_path+"D")
-        if is_safe(arr,n,i,j+1):
-            #move right
-            arr[i][j+1]=0
-            searchMazeHelper(arr,n,i,j+1,output,current_path+"R")
-        if is_safe(arr,n,i-1,j):
-            #move top
-            arr[i-1][j]=0
-            searchMazeHelper(arr,n,i-1,j,output,current_path+"T")
-        if is_safe(arr,n,i,j-1):
-            #move left
-            arr[i][j-1]=0
-            searchMazeHelper(arr,n,i,j-1,output,current_path+"L")
-        #mark as available so that all other paths will be found
-        arr[i][j]=1
+        for d,n_r,n_c in available_directions:
+            if is_safe(arr,n,r+n_r,c+n_c):
+                #-1 is occupied by our move 
+                arr[r][c]=-1
+                current_path += d
+                searchMazeHelper(arr,n,r+n_r,c+n_c,output,current_path)
+                #backtrack
+                arr[r+n_r][c+n_c] = 1
+                current_path = current_path[:-1]
 
 if __name__ == "__main__":
-    # import doctest
-    # doctest.testmod()
+    import doctest
+    doctest.testmod()
     print(searchMaze([[1,0,0,0],[1,1,0,1],[1,1,0,0],[0,1,1,1]],4))
