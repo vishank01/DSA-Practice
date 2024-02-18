0daae5bb9e5e5bd5aa8d02691ad4c066314b736b
vishnu01 <shashankmv01@gmail.com>
2024-02-18 21:29:43 +0530

    added bisect algo with insert intervals

--- topic-wise/grokkingpatterns/04.mergeintervals/02.insertintervals.py
+++ topic-wise/grokkingpatterns/04.mergeintervals/02.insertintervals.py
@@ -31,6 +31,7 @@ Constraints:
     newInterval.length == 2
     0 <= start <= end <= 105
 """
+import bisect
 
 def insert_intervals(intervals:list[list[int]],new_interval:list[int])->list[list[int]]:
     """
@@ -38,6 +39,11 @@ def insert_intervals(intervals:list[list[int]],new_interval:list[int])->list[lis
             1. To check for overlapping condition b<c provided, intervals are sorted by start times
             2. Once overlapping is achieved, then always this condition is satisifed a<=d
                 this condition satisfies 4/6 overlapping cases out of which 2 are completely non overlapping
+    >>> insert_intervals([[1,2],[3,5],[6,7],[8,10],[12,16]],[4,8])
+    [[1, 2], [3, 10], [12, 16]]
+
+    >> insert_intervals([[1,5]],[6,8])
+    [[1, 5], [6, 8]]
 
     """
     i=0
@@ -55,6 +61,41 @@ def insert_intervals(intervals:list[list[int]],new_interval:list[int])->list[lis
         output.append(intervals[i]);i+=1
     return output
             
+def insert_intervals_binary_search(intervals:list[list[int]],new_interval:list[int])->list[list[int]]:
+    """
+        Use binary search twice to find the overlapping intervals.
+        . bisect_left searches the leftmost interval whose end is greater than new interval's start. 
+        . bisect_right searches the rightmost interval whose start is less than the new interval's end. 
+
+        if nums = [1,3,5,7,10,25,25,49,55] and 25 to be inserted,then 
+            bisect_right gives index 7
+            bisect_left gives index 5
+        
+        Note that the right bound right_idx returned by bisect_right is not inclusive. 
+        Thus, if right_idx - left_idx <= 0, then there's no interval overlapped with the new one. 
+        Otherwise, there are at least one overlapping intervals, and we can merge them in O(1) complexity.
+
+        The bisect_left and bisect_right functions in Python's bisect module return sthe same index when the searched element is not present in the list.
+        They differ when the searched element is present in the list. bisect_left returns the leftmost index where the element can be inserted without changing the order of elements, 
+        while bisect_right returns the rightmost index where the element can be inserted without changing the order of elements.
+
+    >>> insert_intervals_binary_search([[1,2],[3,5],[6,7],[8,10],[12,16]],[4,8])
+    [[1, 2], [3, 10], [12, 16]]
+
+    >> insert_intervals_binary_search([[1,5]],[6,8])
+    [[1, 5], [6, 8]]
+    """
+    # intervals < left_idx does not overlap
+    left_idx = bisect.bisect_left(intervals,new_interval[0],key=lambda x:x[1])
+    right_idx = bisect.bisect_right(intervals,new_interval[1],key=lambda x:x[0])
+    # merge new interval with intervals[left_idx:right_idx]
+    if right_idx!=left_idx:
+        new_start = min(new_interval[0], intervals[left_idx][0])
+        new_end = max(new_interval[1], intervals[right_idx-1][1])
+    else:
+        new_start,new_end = new_interval
+    return intervals[:left_idx]+[[new_start,new_end]]+intervals[right_idx:]
 
 if __name__=="__main__":
-    print(insert_intervals([[1,2],[3,5],[6,7],[8,10],[12,16]],[4,8]))
+    import doctest
+    doctest.testmod()
