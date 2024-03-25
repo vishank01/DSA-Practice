400249839ebd673d247d8c8f771bbe584c15cb1a
vishnu01 <shashankmv01@gmail.com>
2024-03-25 20:18:27 +0530

    added another solution with dummy node to avoid edge cases

--- topic-wise/grokkingpatterns/06.inplacereversaloflinkedlist/02.reversesublist.py
+++ topic-wise/grokkingpatterns/06.inplacereversaloflinkedlist/02.reversesublist.py
@@ -45,3 +45,37 @@ class Solution:
         last_node_of_second_part.next = curr_node
         return head
         
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class SolutionWithDummyNodeToAvoidEdgeCases:
+    def reverseBetween(self, head:ListNode|None, left: int, right: int) -> ListNode|None:
+        i=1
+        #introduce dummy node to avoid edge cases
+        dummy_head = ListNode(None,head)
+        curr_node = dummy_head
+        prev_node = None
+        while curr_node and i<=left:
+            prev_node = curr_node
+            curr_node = curr_node.next
+            i+=1
+        #current node is at left
+        last_node_of_first_part = prev_node
+        last_node_of_second_part = curr_node
+
+        prev_node = None
+        while curr_node and i<=right+1:
+            next_node = curr_node.next
+            curr_node.next = prev_node
+
+            prev_node = curr_node
+            curr_node = next_node
+            i+=1
+
+        last_node_of_first_part.next = prev_node
+        last_node_of_second_part.next = curr_node
+        return dummy_head.next
+        
